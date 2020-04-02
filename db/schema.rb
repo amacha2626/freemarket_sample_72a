f44c14a8665ad415bb046f3921bbb101f7b701f7
ky3745 <k.y.wimper@gmail.com>
2020-04-02 12:26:30 +0900

    Revert "test migration"
    This reverts commit 7e7e8dadda2a1d2a1217c009f84e8506d93baead.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20200401101845) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "nickname"
+    t.string   "nickname",                            null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"

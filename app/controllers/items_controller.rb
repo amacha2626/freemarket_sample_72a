a23438e83a58c9ac1436793b91db46a9b447c8a5
minako-h <hawaii77777m@icloud.com>
2020-04-07 18:54:31 +0900

    item-content

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,7 @@ class ItemsController < ApplicationController
     @items = Item.all.order("created_at DESC").limit(3)
   end
 
+  
   def new
   end
 
@@ -11,6 +12,7 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
@@ -24,5 +26,11 @@ class ItemsController < ApplicationController
 
   def buy
   end
+
+  private
+  def item_params
+    params.require(:items).permit(:name, :description, :category_id, :brand_id, :condition, :postage_payer, :shipping_from, :necessary_days, :price, :seller_id)
+  end
+
 
 end

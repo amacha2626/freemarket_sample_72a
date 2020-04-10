5d15e53c71f0b5072465402faa2a138841cd2d74
amacha2626 <amacha2626@gmail.com>
2020-04-10 12:18:56 +0900

    add devise controller

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,9 +3,9 @@ class Profile < ApplicationRecord
   
   
   
-    validates :first_name, presence: true
-    validates :family_name, presence: true
-    validates :first_name_kana, presence: true
-    validates :family_name_kana, presence: true
-    validates :birth_day, presence: true
+  validates :first_name, presence: true
+  validates :family_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :family_name_kana, presence: true
+  validates :birth_day, presence: true
 end

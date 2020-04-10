c06d54cd51594d9c6fcccc1461c7a5378fedb9d7
amacha2626 <amacha2626@gmail.com>
2020-04-10 11:31:01 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -68,6 +68,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :brand_id, :condition, :postage_payer, :shipping_from, :necessary_days, :price, item_images_attributes: [:image]).merge(seller_id: 1)
+    params.require(:item).permit(:name, :description, :category_id, :brand_id, :condition, :postage_payer, :shipping_from, :necessary_days, :price, item_images_attributes: [:image]).merge(seller_id: current_user.id)
   end
 end

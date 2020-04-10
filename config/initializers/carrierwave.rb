e355b51d42ff3bd62e980744f6823609501fbd17
amacha2626 <amacha2626@gmail.com>
2020-04-10 17:29:14 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'furima72a'
+  config.fog_directory = 'furima72a'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/furima72a'
 end

e9d45eba4fc6831d12c49b9534cd7cb66ef0c371
amacha2626 <amacha2626@gmail.com>
2020-04-11 18:44:20 +0900

    Revert "Revert "merge into master""
    This reverts commit 7e71b82ab50f99135c209a91724accdd70a3229f.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+ :registrations => 'users/registrations',
+ :sessions => 'users/sessions'
+}
+  get 'card/new'
+
+  get 'card/show'
   root to: 'items#index'
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy, :buy] do
     resources :item_images, only: [:create, :destroy]
     member do
       get 'buy'
     end
+    collection do
+      get 'category_children', defaults: { format: 'json' }
+      get 'category_grandchildren', defaults: { format: 'json' }
+    end
   end
   resources :users, only: [:show, :edit, :update, :logout] do
-    resources :profiles, only: :create
+    resources :profiles, only: [:new, :create, :show]
     resources :delivery_destinations, only: [:new, :create, :edit, :update]
     member do
       get 'logout'
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+describe User do
+  describe '#create' do
+    it "nicknameがない場合は登録できないこと" do
+     user = build(:user, nickname: nil)
+     user.valid?
+     expect(user.errors[:nickname]).to include("を入力してください。")
+    end
+
+    it "emailがない場合は登録できないこと" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください。")
+    end
+    
+    it "passwordがない場合は登録できないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください。")
+    end
+    
+  end
+end

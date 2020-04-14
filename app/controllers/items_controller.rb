c76cc581f8621875cb4a545b4a7e0edf9b783d14
amacha2626 <amacha2626@gmail.com>
2020-04-14 18:40:02 +0900

    change each to pluck

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,27 +9,16 @@ class ItemsController < ApplicationController
     @item = Item.new
     @item.item_images.new
     @category_parent_array = [["---", ""]]
-    Category.where(ancestry: nil).each do |parent|
-      array = []
-      array << parent.name
-      array << parent.id
-      @category_parent_array << array
-    end
+    @category_parent_array = Category.where(ancestry: nil).pluck(:name, :id)
   end
 
   def create
     @item = Item.new(item_params)
     if @item.save
-
       redirect_to root_path
     else
       @category_parent_array = [["---", ""]]
-      Category.where(ancestry: nil).each do |parent|
-        array = []
-        array << parent.name
-        array << parent.id
-        @category_parent_array << array
-      end
+      @category_parent_array = Category.where(ancestry: nil).pluck(:name, :id)
       render :new
     end
   end
@@ -41,34 +30,13 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
     @item.item_images.find_by(item_id: params[:id])
-    @category_parent_array = [["---", ""]]
-    Category.where(ancestry: nil).each do |parent|
-      array = []
-      array << parent.name
-      array << parent.id
-      @category_parent_array << array
-    end
-
-    @category_children = [["---", ""]]
-    select_category_grandparent_id = Category.find(@item.category_id).parent.parent_id
-    Category.find(select_category_grandparent_id).children.each do |children|
-      array = []
-      array << children.name
-      array << children.id
-      @category_children << array
-    end
-
-    @category_grandchildren = [["---", ""]]
-    select_category_parent_id = Category.find(@item.category_id).parent_id
-    Category.find(select_category_parent_id).children.each do |grandchildren|
-      array = []
-      array << grandchildren.name
-      array << grandchildren.id
-      @category_grandchildren << array
-    end
+    @category_parent_array = Category.where(ancestry: nil).pluck(:name, :id)
 
     @category_grandparent_id = Category.find(@item.category_id).parent.parent_id
+    @category_children = Category.find(@category_grandparent_id).children.pluck(:name, :id)
+
     @category_parent_id = Category.find(@item.category_id).parent_id
+    @category_grandchildren = Category.find(@category_parent_id).children.pluck(:name, :id)
   end
 
   def update
@@ -97,6 +65,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :brand_id, :condition, :postage_payer, :shipping_from, :necessary_days, :price, item_images_attributes: [:image, :_destroy, :id]).merge(seller_id: 1)
+    params.require(:item).permit(:name, :description, :category_id, :brand_id, :condition, :postage_payer, :shipping_from, :necessary_days, :price, item_images_attributes: [:image, :_destroy, :id]).merge(seller_id: current_user.id)
   end
 end

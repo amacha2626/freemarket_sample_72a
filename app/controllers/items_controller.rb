ecaa242943579f8f34986957345097ad3b59fba3
amacha2626 <amacha2626@gmail.com>
2020-04-14 12:20:40 +0900

    エラーハンドリング実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,8 +46,11 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if item.destroy
+      redirect_to root_path
+    else
+      redirect_to edit_item_path
+    end
   end
 
   def buy

f67ac49a0d04545be642d13cb05ab565256eb578
koizs <k64c27f972rgba@icloud.com>
2020-04-17 15:32:24 +0900

    payjp

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,79 +1,49 @@
 class CreditCardsController < ApplicationController
-  before_action :get_user_params, only: [ :edit, :confirmation, :show]
-  before_action :get_payjp_info, only: [:new_create, :create, :delete, :show]
-
   def new
+    card = CreditCard.where(user_id: current_user.id)
+    redirect_to credit_card_path(current_user.id) if card.exists?
   end
 
-  def edit
-    card = current_user.credit_card
-    if card.present?
-      customer = Payjp::Customer.retrieve(credit_cards.costomer_id)
-      @default_card_information = customer.cards.retrieve(credit_cards.card_id)
-    else
-      redirect_to action: "confirmation", id: current_user.id
-    end
-  end
 
-  def create
+  def pay #payjpとCardのデータベース作成
+    Payjp.api_key = Rails.application.secrets[:PAYJP_PRIVATE_KEY]
+    #保管した顧客IDでpayjpから情報取得
     if params['payjp-token'].blank?
-      redirect_to action: "edit", id: current_user.id
+      redirect_to new_card_path
     else
       customer = Payjp::Customer.create(
-      email: current_user.email,
-      card: params['payjp-token'],
-      metadata: {user_id: current_user.id}
-      )
+        card: params['payjp-token'],
+        metadata: {user_id: current_user.id}
+      ) 
       @card = CreditCard.new(user_id: current_user.id, costomer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to root_path
+        redirect_to credit_card_path(current_user.id)
       else
-        redirect_to action: "edit", id: current_user.id
+        redirect_to pay_credit_cards_path
       end
     end
   end
-  
-  def delete
-    card = current_user.credit_card
+
+  def destroy #PayjpとCardデータベースを削除
+    card = CreditCard.find_by(user_id: current_user.id)
     if card.blank?
-      redirect_to action: "new"
     else
-      Payjp.api_key = 'sk_test_0e2eb234eabf724bfaa4e676'
-      customer = Payjp::Customer.retrieve(card.customer_id)
+      Payjp.api_key = Rails.application.secrets[:PAYJP_PRIVATE_KEY]
+      customer = Payjp::Customer.retrieve(card.costomer_id)
       customer.delete
-     #ここでpay.jpの方を消している
       card.delete
-     #ここでテーブルにあるcardデータを消している
-    end  
-      # redirect_to action: "confirmation", id: current_user.id
+    end
+      redirect_to new_credit_card_path
   end
 
-  def show
-    card = current_user.credit_card
-    if card.present?
+  def show #Cardのデータpayjpに送り情報を取り出す
+    card = CreditCard.find_by(user_id: current_user.id)
+    if card.blank?
+      redirect_to new_credit_card_path 
+    else
+      Payjp.api_key = Rails.application.secrets[:PAYJP_PRIVATE_KEY]
       customer = Payjp::Customer.retrieve(card.costomer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
-    else
-      redirect_to action: "confirmation", id: current_user.id
     end
-  end
-
-  def confirmation
-    card = current_user.credit_cards
-    redirect_to action: "show" if card.exists?
-  end
-
-  private
-
-  def get_payjp_info
-    if Rails.env == 'development'
-      Payjp.api_key = 'sk_test_6f9f3ca9ec31150d8902b510'
-    else
-      Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
-    end
-  end
-
-  def get_user_params
-    current_user = User.find(params[:user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
+  get 'buyers/done'
+
   devise_for :users, :controllers => {
  :registrations => 'users/registrations',
  :sessions => 'users/sessions'
 }
-  get 'card/new'
-
-  get 'card/show'
   root to: 'items#index'
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy, :buy] do
+    resources :buyers, only: [:index] do
+      collection do
+        get 'done', to: 'buyers#done'
+        post 'pay', to: 'buyers#pay'
+      end
+    end
     resources :item_images, only: [:create, :destroy]
     member do
       get 'buy'
@@ -24,15 +29,12 @@ Rails.application.routes.draw do
       get 'logout'
       get 'credit'
     end
-    resources :credit_cards, only: [:new, :create, :show, :edit] do
-      collection do
-        post 'delete', to: 'credit_cards#delete'
-        post 'show'
-      end
-      member do
-        get 'confirmation'
-      end
-    end 
   end
+  resources :credit_cards, only: [:new, :show, :destroy] do
+    collection do
+      post 'pay', to: 'credit_cards#pay'
+    end
+  end
+
   resources :categories, only: :index
 end
